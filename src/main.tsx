e413325e73709af4b91d91bd7c5b8ea9b598012f
markpedong <mark.pedong01@gmail.com>
2023-03-22 08:10:33 +0800

    ADDED:
    -basic routing of the application.
    -react-router-dom dependency.
    -main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./pages/app";
 import Root from "./pages/root";
 import "./styles/global.module.scss";
 
+const router = createBrowserRouter([
+  { path: "/", element: <Root /> },
+  { path: "/app", element: <App /> },
+]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Root />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
