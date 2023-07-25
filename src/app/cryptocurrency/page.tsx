e36a1f65b2f59c53ae28a0d9dd7ad41af59c7f28
markpedong <mark.pedong01@gmail.com>
2023-07-25 20:33:19 +0800

    UPDATE:
    - added configs. on tsconfig.json.
    - removed unused page.
    - added test data from cmc.
    - removed searchbar.

--- src/app/cryptocurrency/page.tsx
+++ src/app/cryptocurrency/page.tsx
@@ -1,15 +1,41 @@
-// async function getData() {
-//   const res = await fetch("/v1/global-metrics/quotes/latest", {
-//     headers: {
-//       "X-CMC_PRO_API_KEY": "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c",
-//     },
-//   });
+"use client";
 
-//   return res.json();
-// }
+import { ProColumns, ProTable } from "@ant-design/pro-components";
+
+async function getCryptocurrency() {
+  const res = await fetch(
+    "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+    {
+      headers: {
+        "X-CMC_PRO_API_KEY": "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c",
+      },
+    }
+  );
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const columns: ProColumns[] = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+];
 
 export default async function Cryptocurrency() {
-  // const data = await getData();
+  const getTableData = async () => {
+    const data = await getCryptocurrency();
 
-  return <div>Cryptocurrency</div>;
+    console.log(data.data);
+    return {
+      data: data.data,
+    };
+  };
+
+  return <ProTable columns={columns} request={getTableData} />;
 }
--- src/components/pro-layout.tsx
+++ src/components/pro-layout.tsx
@@ -59,7 +59,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             </Typography.Link>
           );
         }}
-        //actionsRender={() => [renderSearchbar()]}
       >
         {children}
       </ProLayout>
