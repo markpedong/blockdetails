e70ec714f0621b8d0db43233cf68f44275dbaabf
markpedong <mark.pedong01@gmail.com>
2023-08-22 21:24:21 +0800

    UPDATE:
    - added exception for the eslint.
    - added exception for gitignore.
    - update dependencies.
    - typeRoots.
    - update spelling.

--- src/app/cryptocurrency/tokens/page.tsx
+++ src/app/cryptocurrency/tokens/page.tsx
@@ -63,9 +63,9 @@ const Tokens: FC = () => {
 						color="white"
 						title={
 							<span style={{ color: 'black' }}>
-								The total market value of a cryptocurrency's circulating supply. It is analogous to the
-								free-float capitalization in the stock market. Market Cap = Current Price x Circulating
-								Supply.
+								The total market value of a cryptocurrency&apos;s circulating supply. It is analogous to
+								the free-float capitalization in the stock market. Market Cap = Current Price x
+								Circulating Supply.
 								<Typography.Link>Read More</Typography.Link>
 							</span>
 						}
---
+++ src/app/exchanges/spot/detail/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import React, { FC } from 'react'
+
+const Detail: FC = () => {
+	return <div>Detail</div>
+}
+
+export default Detail
